887510d4c2a7724f80040eba29372cf8c4bbed6c
NonnyBP <jdcastellanosbp@live.com>
2016-11-18 23:41:32 -0800

    button to profileActivity
    added button (ID: profileButton) & routed to begin activity 'profile'
    (empty template)

--- app/src/main/java/engifinal/speedshield/MainActivity.java
+++ app/src/main/java/engifinal/speedshield/MainActivity.java
@@ -1,5 +1,6 @@
 package engifinal.speedshield;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,6 +9,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+
 import org.greenrobot.eventbus.EventBus;
 
 public class MainActivity extends AppCompatActivity {
@@ -27,6 +30,11 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+    }
+
+    public void start_profile_activity(View view) {
+        Intent intent = new Intent(this, profile.class);
+        startActivity(intent);
     }
 
     @Override
