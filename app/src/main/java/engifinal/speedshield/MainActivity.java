0bbed474c78d7a6e40b4d72ddd72acc77e5ebe6c
jalbatross <ih8u2o@gmail.com>
2016-11-20 12:42:29 -0800

    HTTP Get for HERE API working

--- app/src/main/java/engifinal/speedshield/MainActivity.java
+++ app/src/main/java/engifinal/speedshield/MainActivity.java
@@ -15,7 +15,13 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
-import com.google.android.gms.common.GooglePlayServicesUtil;
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import javax.net.ssl.HttpsURLConnection;
 import org.greenrobot.eventbus.EventBus;
 
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
@@ -34,13 +40,64 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
+            public void onClick(View view)  {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+                String url = "http://www.google.com/search?q=mkyong";
+
+
+                Thread thread = new Thread(new Runnable() {
+
+                    @Override
+                    public void run() {
+                        try  {
+                            httpGet();
+                        } catch (Exception e) {
+                            //e.printStackTrace();
+                        }
+                    }
+                });
+
+                thread.start();
             }
         });
     }
 
+    public void httpGet() throws Exception {
+        try {
+            String url = "https://route.cit.api.here.com/routing/7.2/getlinkinfo.json?waypoint=52.5308%2C13.3846&app_id=ZQJA8fSBp8MZzFFMFcF8&app_code=Hobr4vTPttml3HNkFByD2g";
+
+            URL obj = new URL(url);
+            HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+
+            // optional default is GET
+            con.setRequestMethod("GET");
+
+            //add request header
+            //con.setRequestProperty("User-Agent", USER_AGENT);
+
+            int responseCode = con.getResponseCode();
+            System.out.println("\nSending 'GET' request to URL : " + url);
+            System.out.println("Response Code : " + responseCode);
+
+            BufferedReader in = new BufferedReader(
+                    new InputStreamReader(con.getInputStream()));
+            String inputLine;
+            StringBuffer response = new StringBuffer();
+
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+            in.close();
+
+            //print result
+            System.out.println(response.toString());
+        }
+        catch(Exception e) {
+            System.out.println("exception caught undhandled");
+            System.out.println(e + " was the exception");
+        }
+    }
     public void start_profile_activity(View view) {
         Intent intent = new Intent(this, profile.class);
         startActivity(intent);
